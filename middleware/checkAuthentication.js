439be074d05d91041c10b6c639cc1a8191d4bdf3
Mateo Caktas <mcakta00@fesb.hr>
2020-07-20 08:48:27 +0200

    Fixed ESLint issues

--- middleware/checkAuthentication.js
+++ middleware/checkAuthentication.js
@@ -1,9 +1,9 @@
 'use strict';
 
-require('dotenv').config()
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-const signJwt = (data) => {
+const signJwt = data => {
   const email = data.email;
   const user = { name: email };
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
@@ -12,16 +12,16 @@ const signJwt = (data) => {
 };
 
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
+  const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(' ')[1];
-  if(token == null) return res.sendStatus(401);
+  if (token == null) return res.sendStatus(401);
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if(err) return res.sendStatus(403);
+    if (err) return res.sendStatus(403);
 
     req.user = user;
     next();
-  })
+  });
 };
 
 module.exports = {
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const { signJwt, authenticateToken } = require('../../middleware/checkAuthentication');
-require('dotenv').config()
+const { authenticateToken, signJwt } = require('../../middleware/checkAuthentication');
+require('dotenv').config();
 // POST login metoda
 module.exports = function (app, passport) {
-  app.get('/login', (req, res, next) => {
+  app.post('/login', (req, res, next) => {
     console.log(req.body);
     passport.authenticate('local', {
       successRedirect: '/dashboard',
@@ -20,9 +20,6 @@ module.exports = function (app, passport) {
     res.json({ myJwt });
   });
 
-  app.get('/testToken', authenticateToken, (req, res, next) => {
-    res.send('This is very much authenticated!');
-  })
   app.get('/failure', (req, res) => {
     console.log('Been to failure');
     res.send('Fail');
